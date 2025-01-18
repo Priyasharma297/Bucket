8e778e4f443e5dd0913cdcfe2634119028d43fa8
Priyasharma297 <146953675+Priyasharma297@users.noreply.github.com>
2025-01-18 19:46:03 +0530

    hmm

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ app.use('/bucket', require('./routes/bucket'));
 app.use('/api', require('./routes/itinerary'));
 
 const PORT=process.env.PORT;
-app.listen(PORT, () => {
+app.listen(5000, () => {
     console.log('Server started on port 5000 => http://localhost:5000');
 });
